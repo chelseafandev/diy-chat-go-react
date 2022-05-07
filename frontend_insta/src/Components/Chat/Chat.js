86c357bcf3348d78f1ebe985a2d95cf708197138
chelseafandev <chelseafandev@gmail.com>
2022-05-08 03:01:48 +0900

    chore: react 문법 관련 주석 추가 - useState - cleanup function - useEffect - props

--- frontend_insta/src/Components/Chat/Chat.js
+++ frontend_insta/src/Components/Chat/Chat.js
@@ -9,6 +9,7 @@ import TextContainer from "../TextContainer/TextContainer"
 let socket;
 
 function Chat({ location }) {
+    // re-rendering을 위한 효과적인 방법은? useState!
     const [name, setName] = useState("");
     const [room, setRoom] = useState("");
     const [message, setMessage] = useState("");
@@ -18,6 +19,8 @@ function Chat({ location }) {
     // useEffect 안에서 사용하는 상태나, props 가 있다면, useEffect 의 deps 에 넣어주어야 합니다. 그렇게 하는게, 규칙입니다.
     // 만약 useEffect 안에서 사용하는 상태나 props 를 deps 에 넣지 않게 된다면 useEffect 에 등록한 함수가 실행 될 때,
     // 최신 props/상태를 가르키지 않게 됩니다.
+    //
+    // deps에 location.search를 추가했다는 의미 = location.search 값이 변경될때만 아래 내용을 실행하겠다는 의미
     useEffect(() => {
         // 컴포넌트가 화면에서 나타남
         socket = new WebSocket("ws://localhost:8080/ws");
@@ -40,7 +43,7 @@ function Chat({ location }) {
             );
         };
         
-        // 컴포넌트가 화면에서 사라짐
+        // 컴포넌트가 파괴될 때 호출될 함수(cleanup function)
         return () => {
             socket.close(1000, "socket close...");
             console.log("socket close...");
--- frontend_insta/src/Components/Messages/Messages.js
+++ frontend_insta/src/Components/Messages/Messages.js
@@ -3,6 +3,8 @@ import ScrollToBottom from "react-scroll-to-bottom";
 import "./Messages.css"
 import Message from "./Message/Message"
 
+// props 는 부모 컴포넌트로부터 자식 컴포넌트에 데이터를 보낼 수 있게 해주는 방법
+// props 자체를 인자로 넘길 수 있지만 property 변수(여기서는 messages와 name)를 직접 입력해서 넘겨주는게 일반적임
 function Messages({ messages,name }) {
   return (
     <ScrollToBottom className = "messages">
